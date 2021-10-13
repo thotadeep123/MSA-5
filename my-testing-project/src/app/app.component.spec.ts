90df890ec9dbe5174ff3d341fb5d1cdd3dc46030
thotadeep123 <thotadeep89@gmail.com>
2021-10-13 16:12:43 +0530

    msa-5

--- my-testing-project/src/app/app.component.spec.ts
+++ my-testing-project/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-frontend');
+    expect(app.title).toEqual('Employee Management System');
   });
 
   it('should render title', () => {
